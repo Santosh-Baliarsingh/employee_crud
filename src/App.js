b4733470d4e64b3af9773f317fcb28aabedb1964
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-06-09 22:08:23 +0530

    App.js Updated

--- src/App.js
+++ src/App.js
@@ -12,27 +12,46 @@ const employees = [
   { eId: 104, name: "Yuno", sal: 70000 },
 ];
 
+const initialFormState = {eId : null , name : "" , sal : null};
+
 function App() {
   const [isEdit, setIsEdit] = useState(false);
 
   const [empArr, setEmpArr] = useState(employees);
 
-  // Delete Emplyee Functionality Here
+  const [currentEmp, setCurrentEmp] = useState({initialFormState});
+
+  // deleteEmplyee Functionality Here
   const deleteEmployee = (eId) => {
     const filteredArr = empArr.filter((emp) => emp.eId !== eId);
 
     setEmpArr([...filteredArr]);
   };
 
-  const addNewEmployee = (empObj) =>{
-
-    setEmpArr([...empObj,empObj]);
+  // addNewEmployee Functionality Here
+  const addNewEmployee = (empObj) => {
+    // debugger;
+    setEmpArr([...empArr, empObj]);
   };
+
+  // EditEmployee Functionality Here
+  const editEmployee = (eId) => {
+    setIsEdit(true)
+    const selectedEmp = empArr.find((emp) => emp.eId === eId);
+    setCurrentEmp(selectedEmp);
+  };
+  
+  // updateEmployee functionality Here
+  const updateEmployee = (eId , updateEmpObj) =>{
+    setIsEdit(false);
+    setEmpArr(empArr.map((emp) => (emp.eId === eId ? updateEmpObj : emp)))
+
+  }
 
   return (
     <>
       <h2 className="text-center text-light bg-dark p-2">
-        Employee CRUD Application
+        Employee CRUD Application Using Functional Component
       </h2>
       <div className="container">
         <div className="row">
@@ -40,10 +59,19 @@ function App() {
             <EmployeeLists
               empArr={empArr}
               deleteEmployee={deleteEmployee}
+              editEmployee={editEmployee}
             ></EmployeeLists>
           </div>
           <div className="col-sm-4">
-            {isEdit ? <AddEmployee /> : <AddEmployee addNewEmployee={addNewEmployee} />}
+            {isEdit ? (
+              <EditEmployee currentEmp={currentEmp}
+              isEdit = {isEdit}
+              setIsEdit = {setIsEdit}
+              updateEmployee = {updateEmployee}
+               />
+            ) : (
+              <AddEmployee addNewEmployee={addNewEmployee} />
+            )}
           </div>
         </div>
       </div>
