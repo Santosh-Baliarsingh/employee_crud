900341e9b5819e4577700aceccf5135665e01a47
santosh Baliarsingh <santoshbaliarsingh1997@gmail.com>
2022-06-10 11:21:36 +0530

    Components AddEmployee.js and EditEmployee.js Updated

--- src/components/AddEmployee.js
+++ src/components/AddEmployee.js
@@ -1,52 +1,63 @@
 import React, { useState } from "react";
 
 export default function AddEmployee(props) {
-  const [empObj, setEmpObj] = useState({});
+  const [eId, seteId] = useState(0);
+
+  const [name, setName] = useState("");
+
+  const [sal, setSal] = useState(0);
+
   return (
     <>
       <div className="text-center fw-bold h4">AddEmployee Form</div>
-      <form>
-        <div class="mb-3">
-          <label for="exampleInputEmployeeId" class="form-label fw-bold">
-            Employee ID
-          </label>
-          <input
-            type="number"
-            class="form-control"
-            id="exampleInputEmpId"
-            aria-describedby="emailHelp"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputEmployeeName" class="form-label fw-bold">
-            Employee Name
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            id="exampleInputEmpName"
-            aria-describedby="emailHelp"
-          />
-        </div>
-        <div class="mb-3">
-          <label for="exampleInputEmployeeSalary" class="form-label fw-bold">
-            Employee Salary
-          </label>
-          <input
-            type="number"
-            class="form-control"
-            id="exampleInputEmpSal"
-            aria-describedby="emailHelp"
-          />
-        </div>
-        <button
-        onClick={() =>{ props.addNewEmployee(empObj)}}
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmployeeId" className="form-label fw-bold">
+          Employee ID
+        </label>
+        <input
+          onChange={(e) => {
+            seteId(e.target.value);
+          }}
+          type="number"
+          className="form-control"
+          id="exampleInputEmpId"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmployeeName" className="form-label fw-bold">
+          Employee Name
+        </label>
+        <input
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+          type="text"
+          className="form-control"
+          id="exampleInputEmpName"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmployeeSalary" className="form-label fw-bold">
+          Employee Salary
+        </label>
+        <input
+          onChange={(e) => {
+            setSal(e.target.value);
+          }}
+          type="number"
+          className="form-control"
+          id="exampleInputEmpSal"
+        />
+      </div>
+      <button
+        onClick={() => {
+          const empObj = { eId, name, sal };
+          props.addNewEmployee(empObj);
+        }}
         className="btn btn-primary "
       >
         Add New Employee
       </button>
-      </form>
-     
     </>
   );
 }
--- src/components/EditEmployee.js
+++ src/components/EditEmployee.js
@@ -1,7 +1,80 @@
-import React from 'react'
+import React, {  useState } from "react";
 
-export default function EditEmployee() {
+export default function EditEmployee(props) {
+
+    const [emp , setEmp] = useState(props.currentEmp)
+
+    // useEffect(() =>{
+
+    //     setEmp(props.currentEmp);
+
+    // },[props])
+
+    const updateChange = (e) =>{
+
+        const {name , value} = e.target;
+
+        setEmp({...emp ,[name] : value})
+
+    }
   return (
-    <div>EditEmployee</div>
-  )
+    <>
+      <div className="text-center fw-bold h4">Edit Employee Form</div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmployeeId" className="form-label fw-bold">
+          Employee ID
+        </label>
+        <input
+        onChange={updateChange}
+          name="eId"
+          value={emp.eId}
+          type="number"
+          className="form-control"
+          id="exampleInputEmpId"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmployeeName" className="form-label fw-bold">
+          Employee Name
+        </label>
+        <input
+          onChange={updateChange}
+          name="name"
+          value={emp.name}
+          type="text"
+          className="form-control"
+          id="exampleInputEmpName"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmployeeSalary" className="form-label fw-bold">
+          Employee Salary
+        </label>
+        <input
+          onChange={updateChange}
+          name="sal"
+          value={emp.sal}
+          type="number"
+          className="form-control"
+          id="exampleInputEmpSal"
+        />
+      </div>
+      <button
+        onClick={() => {
+          props.updateEmployee(emp.eId , emp);
+        }}
+        className="btn btn-primary me-4 "
+      >
+        Update Employee
+      </button>
+      <button
+        onClick={() => {
+          props.setIsEdit(false);
+        }}
+        className="btn btn-danger "
+      >
+        Cancel
+      </button>
+    </>
+  );
 }
